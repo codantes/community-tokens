37c8b646e290c0bb4f2b7bb84ad257e9fd2050f4
codantes <pranavismic@gmail.com>
2022-10-05 12:24:43 +0200

    modified helper config

--- deploy/01-deploy-tokens.js
+++ deploy/01-deploy-tokens.js
@@ -1,15 +1,21 @@
 const {network, ethers, deployments, getNamedAccounts} = require("hardhat")
+const {VERIFICATION_BLOCK_CONFIRMATIONS} = require("../hardhat-helper-config")
 const {verify} = require("../utils/verify");
 
 module.exports = async({deployments, getNamedAccounts}) => {
     const {deploy, log} = deployments;
     const {deployer} = await getNamedAccounts();
     chainId = network.config.chainId
+
+    const waitBlockConfirmations = (chainId === 31337)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS
+
     const cnt = await deploy("CryptoNaukriToken", {
         from: deployer,
         log: true,
         args:[],
-        waitConfirmations: 1
+        waitConfirmations: waitBlockConfirmations
     })
     console.log(`contract deployed at ${cnt.address}!`)
     console.log("------------------------------------------------")
--- hardhat-helper-config.js
+++ hardhat-helper-config.js
@@ -4,13 +4,15 @@ const networkConfig = {
     },
     42: {
         name: "kovan",
-        ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+        VERIFICATION_BLOCK_CONFIRMATIONS: 5
     },
     4: {
         name: "rinkeby",
-        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        VERIFICATION_BLOCK_CONFIRMATIONS: 5
     },
 }
+
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
 
 const developmentChains = ["hardhat", "localhost"]
 
